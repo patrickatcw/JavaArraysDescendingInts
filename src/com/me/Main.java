a2ffe42843bb949e85ca4522a0f41542175013a7
patrickatcw <pj8392@yahoo.com>
2018-03-02 19:28:09 -0500

    initial structure, directions, updated gitignore file

---
+++ src/com/me/Main.java
@@ -0,0 +1,23 @@
+package com.me;
+//directions
+/*
+- create a program using arrays that sort a list of integers in descending order
+- descending order is highest value to lowest
+- in other words, if the array had the values in it 106, 26, 81, 5, 15, your program should
+ultimately have an array with 106, 81, 26, 15, 5 in it.
+- set up the program so that the numbers to sort are read in from the keyboard
+- implement the following methods - getIntegers, printArray, and sortIntegers
+- getIntegers returns an array of entered integers from keyboard
+- printArray prints out the contents of the array
+and sortIntegers should sort the array and return a new array containing the sorted numbers
+-you will have to fugure out how to copy the array elements from the passed array into a new
+array and sort them and return the new sorted array
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
